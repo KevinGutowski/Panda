e1beb0c0c5109dee7e649732d37de163b1d95707
Kevin Gutowski <kskiviolin@gmail.com>
2020-08-04 00:52:09 -0700

    Initial Commit

---
+++ Shared/PandaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PandaApp.swift
+//  Shared
+//
+//  Created by Kevin Gutowski on 8/4/20.
+//
+
+import SwiftUI
+
+@main
+struct PandaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
