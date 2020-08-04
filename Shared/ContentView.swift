4f831a43563ecd2be7d405e404d670a7e8491a0d
Kevin Gutowski <kskiviolin@gmail.com>
2020-08-04 11:13:41 -0700

    Iniital commit

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,11 +6,45 @@
 //
 
 import SwiftUI
+import AVFoundation
+
+struct Player {
+    let word: String
+    var url: URL?
+    
+    init(word: String) {
+        self.word = word
+        
+        if let path = Bundle.main.path(forResource: "\(word).mp3", ofType: nil) {
+            self.url = URL(fileURLWithPath: path)
+        } else {
+            self.url = nil
+        }
+    }
+    
+    func play() {
+        do {
+            var avplayer: AVAudioPlayer?
+            if let fileURL = self.url {
+                avplayer = try AVAudioPlayer(contentsOf: fileURL)
+                avplayer?.play()
+            }
+        } catch {
+            print("Couldn't play or load file")
+        }
+    }
+}
+
+let hello = Player(word: "ありがとう")
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!").padding()
-    }
+        var body: some View {
+            Button("Play") {
+                print(hello.word)
+                print(hello.url ?? "no url")
+                hello.play()
+            }
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -18,3 +52,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
